38f9fcd753ceac0b309fb8fa166c49fe64e192a5
shubh agrwal <shubhagarwal123123@gmail.com>
2023-01-06 11:46:36 +0530

    added crons schedule

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express=require("express");
 const bodyParser=require("body-parser");
 const {PORT}=require("./config/serverconfig")
-const {sendBasicEmail}=require('./services/emailservice');
+//const {sendBasicEmail}=require('./services/emailservice');
+
+const cron = require('node-cron');
 const setupandstartservice=async()=>{
     const app=express();
     app.use(bodyParser.json);
@@ -10,12 +12,15 @@ const setupandstartservice=async()=>{
     app.listen(PORT,()=>{
         console.log(`server starting at ${PORT}`);
        // console.log(process.env);
-       sendBasicEmail(
-        'support@admin.com',
-        'shubhcod1@gmail.com',
-        'this is a tester mail',
-        'this we will be able to lead the team',
-       )
+    //    sendBasicEmail(
+    //     '"support"<support@admin.com>',
+    //     'shubhcod1@gmail.com',
+    //     'this is a tester mail',
+    //     'this we will be able to lead the team',
+    //    )
+    cron.schedule('*/2 * * * *', () => {
+        console.log('running a task every two minutes');
+      });
     })
 }
 setupandstartservice();
--- src/services/emailservice.js
+++ src/services/emailservice.js
@@ -1,15 +1,18 @@
-const sender=require('../config/emailconfig');
-
-const sendBasicEmail=(from,to,subject,mailBody)=>{
-    sender.sendMail({
-        from:from,
-        to:to,
-        subject:subject,
-        text:mailBody
-   })
-}
-
-
-module.exports={
-    sendBasicEmail
-}
+const sender = require("../config/emailconfig");
+const sendBasicEmail = async (from, to, subject, mailBody) => {
+  try {
+    const response=await sender.sendMail({
+      from: from,
+      to: to,
+      subject: subject,
+      text: mailBody,
+    });
+    console.log(response);
+  } 
+  catch (error) {
+    console.log(error);
+  }
+};
+module.exports = {
+  sendBasicEmail,
+};
